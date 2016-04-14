5936660f2351decb1d8a0531caab09b64fa741c3
Muhammad Saqib Sarwar <saqib@inspirythemes.com>
2016-04-14 13:56:33 +0500

    updated meta box plugin

--- plugins/meta-box/inc/fields/checkbox-list.php
+++ plugins/meta-box/inc/fields/checkbox-list.php
@@ -1,37 +1,19 @@
 <?php
-// Prevent loading this file directly
-defined( 'ABSPATH' ) || exit;
-
-if ( ! class_exists( 'RWMB_Checkbox_List_Field' ) )
+/**
+ * Checkbox list field class.
+ */
+class RWMB_Checkbox_List_Field extends RWMB_Input_List_Field
 {
-	class RWMB_Checkbox_List_Field extends RWMB_Field_Multiple_Values
+	/**
+	 * Normalize parameters for field
+	 * @param array $field
+	 * @return array
+	 */
+	static function normalize( $field )
 	{
-		/**
-		 * Get field HTML
-		 *
-		 * @param mixed $meta
-		 * @param array $field
-		 *
-		 * @return string
-		 */
-		static function html( $meta, $field )
-		{
-			$meta = (array) $meta;
-			$html = array();
-			$tpl  = '<label><input type="checkbox" class="rwmb-checkbox-list" name="%s" value="%s"%s> %s</label>';
+		$field['multiple'] = true;
+		$field = parent::normalize( $field );		
 
-			foreach ( $field['options'] as $value => $label )
-			{
-				$html[] = sprintf(
-					$tpl,
-					$field['field_name'],
-					$value,
-					checked( in_array( $value, $meta ), 1, false ),
-					$label
-				);
-			}
-
-			return implode( '<br>', $html );
-		}
+		return $field;
 	}
 }
